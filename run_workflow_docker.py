3b0404f694040e175d1058915cfdaca02f540267
zaiyan-alam <105238025+zaiyan-alam@users.noreply.github.com>
2023-05-10 07:28:39 -0700

    Fixed bugs in the workflow, added prediction and evaluation scripts, other updates

--- run_workflow_docker.py
+++ run_workflow_docker.py
@@ -1,32 +1,21 @@
-#!/usr/bin/env python3
-
-"""
-
-MASK DETECTION AND CLASSIFICATION WORKFLOW
-
-
-"""
 import re
 import glob,os
 import pickle
 import logging
-
-from Pegasus.api import *
-
+from pathlib import Path
 from utils.wf import split_data_filenames, create_ann_list,create_augmented_filelist
 
-
-# --- Import Pegasus API ---
-from Pegasus.api import *
 logging.basicConfig(level=logging.DEBUG)
-props = Properties()
-# set for checkpointing - if jobs fails or timeouts, Pegasus will retry the job 2 times
-# and use the checkpoint to restart the job
-props["dagman.retry"] = "1"
-props.write()
+
+# --- Import Pegasus API -----------------------------------------------------------
+from Pegasus.api import *
+
+# --- Top Directory Setup ----------------------------------------------------------
+top_dir = Path(__file__).parent.resolve()
 
 # DATA AQUSITION
 imagesList = glob.glob('data/images/*.png')
+predict_images = glob.glob('data/images/pred_imgs/*.png')
 annotationList = glob.glob('data/annotations/*.xml')
 
 NUM_TRIALS = 1
@@ -35,14 +24,20 @@ NUM_EPOCHS = 1
 #DATA SPLIT
 train_filenames,val_filenames,test_filenames, files_split_dict = split_data_filenames(imagesList)
 
-#TODO: check the correctness of the fun
+#ANNOTATIONS
 train_imgs, train_ann = create_ann_list(train_filenames)
 val_imgs, val_ann     = create_ann_list(val_filenames)
 test_imgs, test_ann   = create_ann_list(test_filenames)
 
+######################################## PROPERTIES ###########################################################
+props = Properties()
+props["dagman.retry"] = "1"
+props["pegasus.mode"] = "development"
+props.write()
+
+
 ###################################### REPLICA CATALOG ###########################################################
 
-#add images from group data
 rc = ReplicaCatalog()
 
 inputFiles = []
@@ -50,6 +45,13 @@ for img in imagesList:
     fileName = img.split("/")[-1]
     img_file = File(fileName)
     inputFiles.append(img_file)
+    rc.add_replica("local", img_file,  os.path.join(os.getcwd(),str(img)))
+    
+pred_imgs = []
+for img in predict_images:
+    fileName = img.split("/")[-1]
+    img_file = File(fileName)
+    pred_imgs.append(img_file)
     rc.add_replica("local", img_file,  os.path.join(os.getcwd(),str(img)))
     
 annFiles = []
@@ -60,7 +62,6 @@ for ann in annotationList:
     rc.add_replica("local", ann_file,  os.path.join(os.getcwd(),str(ann)))
     
 ## add checkpointing file for the hpo model job
-
 def create_pkl(model):
     pkl_filename = "hpo_study_" + model + ".pkl"
     file = open(pkl_filename, 'ab')
@@ -71,8 +72,11 @@ mask_detection_pkl = create_pkl("mask_detection")
 mask_detection_pkl_file = File(mask_detection_pkl)
 rc.add_replica("local", mask_detection_pkl, os.path.join(os.getcwd(), mask_detection_pkl))
 
-rc.write()
+fastRCNNP_pkl = create_pkl("fastRCNNP")
+fastRCNNP_pkl_file = File(fastRCNNP_pkl)
+rc.add_replica("local", fastRCNNP_pkl, os.path.join(os.getcwd(), fastRCNNP_pkl))
 
+rc.write()
 
 ###################################### TRANSFORMATIONS ###########################################################
 
@@ -80,9 +84,9 @@ rc.write()
 tc = TransformationCatalog()
 mask_detection_wf_cont = Container(
                 "mask_detection_wf",
-                Container.DOCKER,
-                image="docker://patkraw/mask-detecton-wf:latest",
-                arguments="--runtime=nvidia --shm-size=1gb"
+                Container.SINGULARITY,
+                image="docker://zaiyancse/mask-detection:latest",
+                image_site="docker_hub"
             )
 
 tc.add_containers(mask_detection_wf_cont)
@@ -90,49 +94,64 @@ tc.add_containers(mask_detection_wf_cont)
 
 dist_plot = Transformation(
                 "dist_plot",
-                site = "condorpool",
-                pfn = "/app/bin/plot_class_distribution.py",
-                is_stageable = False,
+                site = "local",
+                pfn = top_dir/"bin/plot_class_distribution.py",
+                is_stageable = True,
                 container = mask_detection_wf_cont 
             )
 
 augment_imgs = Transformation(
                 "augment_images",
-                site = "condorpool",
-                pfn = "/app/bin/data_aug.py",
-                is_stageable = False,
+                site = "local",
+                pfn = top_dir/"bin/data_aug.py",
+                is_stageable = True,
                 container = mask_detection_wf_cont 
             )
+
 rename_imgs = Transformation(
                 "rename_images",
-                site = "condorpool",
-                pfn = "/app/bin/rename_file.py",
-                is_stageable = False,
+                site = "local",
+                pfn = top_dir/"bin/rename_file.py",
+                is_stageable = True,
                 container = mask_detection_wf_cont 
             )
 
 hpo_model = Transformation(
                 "hpo_script",
-                site = "condorpool",
-                pfn = "/app/bin/hpo_train.py",
-                is_stageable = False,
+                site = "local",
+                pfn = top_dir/"bin/hpo_train.py",
+                is_stageable = True,
                 container = mask_detection_wf_cont 
             )
 
 train_model = Transformation(
                 "train_script",
-                site = "condorpool",
-                pfn = "/app/bin/train_model.py",
-                is_stageable = False,
+                site = "local",
+                pfn = top_dir/"bin/train_model.py",
+                is_stageable = True,
+                container = mask_detection_wf_cont 
+            )
+
+evaluate_model = Transformation(
+                "evaluate_script",
+                site = "local",
+                pfn = top_dir/"bin/evaluate.py",
+                is_stageable = True,
+                container = mask_detection_wf_cont 
+            )
+
+predict_detection = Transformation(
+                "predict_script",
+                site = "local",
+                pfn = top_dir/"bin/predict.py",
+                is_stageable = True,
                 container = mask_detection_wf_cont 
             )
 
 
-tc.add_transformations(augment_imgs, dist_plot, rename_imgs,hpo_model, train_model)
+tc.add_transformations(augment_imgs, dist_plot, rename_imgs, hpo_model, train_model, evaluate_model, predict_detection)
 logging.info("writing tc with transformations: {}, containers: {}".format([k for k in tc.transformations], [k for k in tc.containers]))
 tc.write()
-
-
 
 ###################################### CREATE JOBS ###########################################################
 wf = Workflow("mask_detection_workflow")
@@ -142,76 +161,68 @@ distribution_plot_file = File("class_distribution.png")
 val_preprocessed_files = [File("val_"+ f.split("/")[-1]) for f in val_filenames]
 test_preprocessed_files = [File("test_"+ f.split("/")[-1]) for f in test_filenames]
 
-# DATA EXPLORATION
-# takes in all the annotationa files and creates plot with distribution of the classes
 distribution_plot_job = Job(dist_plot)
+distribution_plot_job.add_args(distribution_plot_file)
 distribution_plot_job.add_inputs(*train_ann, *val_ann, *test_ann)
 distribution_plot_job.add_outputs(distribution_plot_file)
+wf.add_jobs(distribution_plot_job)
 
-# DATA PREPROCESSING:TRAIN DATA-DATA AUGMENTATION
-# takes images and adds gaussian noise to them
+# TRAIN DATA AUGMENTATION
 preprocess_train_job = Job(augment_imgs)
 preprocess_train_job.add_inputs(*train_imgs)
-preprocess_train_job.add_outputs(*train_preprocessed_files)
+preprocess_train_job.add_outputs(*train_preprocessed_files,stage_out=False)
+wf.add_jobs(preprocess_train_job)
 
-# DATA PREPROCESSING:VAL DATA-FILE RENAMING
+# VAL DATA-FILE RENAMING
 preprocess_val_job = Job(rename_imgs)
 preprocess_val_job.add_inputs(*val_imgs)
-preprocess_val_job.add_outputs(*val_preprocessed_files)
+preprocess_val_job.add_outputs(*val_preprocessed_files,stage_out=False)
 preprocess_val_job.add_args("val")
+wf.add_jobs(preprocess_val_job)
 
-# DATA PREPROCESSING:TEST DATA-FILE RENAMING
+# TEST DATA-FILE RENAMING
 preprocess_test_job = Job(rename_imgs)
 preprocess_test_job.add_inputs(*test_imgs)
-preprocess_test_job.add_outputs(*test_preprocessed_files)
+preprocess_test_job.add_outputs(*test_preprocessed_files,stage_out=False)
 preprocess_test_job.add_args("test")
+wf.add_jobs(preprocess_test_job)
 
-# HYPERPARAMETER OPTIMIZATION
-
+hpo_params = File("best_hpo_params.txt")
 hpo_job = Job(hpo_model)
-hpo_job.add_args("--epochs",NUM_EPOCHS, "--trials", NUM_TRIALS)
+hpo_job.add_args("--epochs",NUM_EPOCHS, "--trials", NUM_TRIALS,"--results_file",hpo_params)
 hpo_job.add_inputs(*train_preprocessed_files,*train_ann,*val_preprocessed_files,*val_ann)
-hpo_job.add_outputs(File("best_hpo_params.txt"))
+hpo_job.add_outputs(hpo_params)
 hpo_job.add_checkpoint(mask_detection_pkl_file, stage_out=True)
-hpo_job.add_profiles(Namespace.PEGASUS, key="maxwalltime", value=240)
+wf.add_jobs(hpo_job)
 
+model_file = File("mask_detection_model.pth")
+model_training_job = Job(train_model)
+model_training_job.add_args(hpo_params,model_file)
+model_training_job.add_inputs(hpo_params,*train_imgs,
+                              *train_preprocessed_files, *val_preprocessed_files,
+                              *test_preprocessed_files, *annFiles)
+model_training_job.add_checkpoint(fastRCNNP_pkl_file, stage_out=True)
+model_training_job.add_outputs(model_file)
+wf.add_jobs(model_training_job)
 
-# MODEL TRAINING
-#model_training_job = Job(train_script)
-#model_training_job.add_inputs(*train_imgs)
-#model_training_job.add_outputs(*train_preprocessed_files)
-#model_training_job.add_checkpoint(fastRCNNP_pkl_file, stage_out=True)
-#model_training_job.add_profiles(Namespace.PEGASUS, key="maxwalltime", value=2)
+confusion_matrix_file = File("confusion_matrix.png")
+evaluation_file = File("evaluation.txt")
+model_evaluating_job = Job(evaluate_model)
+model_evaluating_job.add_args(model_file,evaluation_file,confusion_matrix_file)
+model_evaluating_job.add_inputs(model_file,*test_preprocessed_files, *annFiles)
+model_evaluating_job.add_outputs(evaluation_file,confusion_matrix_file)
+wf.add_jobs(model_evaluating_job)
 
+predicted_image = File("predicted_image.png")
+predicted_classes = File("predictions.txt")
+predict_detection_job.add_args(model_file,predicted_image,predicted_classes)
+predict_detection_job.add_inputs(model_file,*pred_imgs, *annFiles)
+predict_detection_job.add_outputs(predicted_image,predicted_classes)
+wf.add_jobs(predict_detection_job)
 
-# MODEL EVALUATION
-
-
-# INFERENCE
-# takes images of our labmates and classifies them
-
-
-
-
-###################################### RUN WORKFLOW ###########################################################
-def main():
-	
-	graph_filename = "mask_detection.dot"
-	
-	wf.add_jobs(
-	    distribution_plot_job,
-	    preprocess_train_job,
-	    preprocess_val_job,
-	    preprocess_test_job,
-	    hpo_job
-	)
-	try:
-		wf.plan(submit=True)
-		wf.wait()
-		wf.statistics()
-		wf.graph(include_files=True, no_simplify=True, label="xform-id", output = graph_filename)
-	except PegasusClientError as e:
-	    print(e.output)
-
-if __name__ == "__main__":
-    main()
+try:
+    wf.plan(submit=True)
+except PegasusClientError as e:
+    print(e.output)
+    
+    
